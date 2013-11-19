ae6be486753f4316f6ec966fe96cb5d59f5f97b6
Ryn830 <ryn830@gmail.com>
2013-11-19 00:20:18 -0800

    [broken-pokemon] played around with jquery, added animate.css, created some Constructor-specific classes

--- src/pokemonDancer.js
+++ src/pokemonDancer.js
@@ -9,5 +9,32 @@ PokemonDancer.prototype.constructer = PokemonDancer;
 
 PokemonDancer.prototype.step = function(){
   this.oldStep();
-  // this.$node.toggle();
+  // var colors = ['DeepSkyBlue',
+  //               'SpringGreen',
+  //               'DarkSlateBlue',
+  //               'DarkRed',
+  //               'SandyBrown'];
+  // var randColor = colors[Math.floor(Math.random() * 5)];
+  // // console.log(randColor);
+  // this.$node.css('color', randColor);
+  this.$node.toggle;
+
+  var stepwise = true;
+  var that = this;
+  var expand = function(){
+    if (stepwise){
+      that.$node.animate({
+        border-radius:'150%'
+      });
+      stepwise = !stepwise;
+    } else {
+      that.$node.animate({
+        border-radius:'33%'
+      });
+      stepwise = !stepwise;
+    }
+    }
+  };
+
+  expand();
 };
